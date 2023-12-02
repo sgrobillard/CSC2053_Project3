1e50e9d69b5665c7ba423c15cd802f5061b571f2
sgrobillard <srobilla@villanova.edu>
2023-12-02 18:41:24 -0500

    With api call

--- project3/src/TriviaPage.js
+++ project3/src/TriviaPage.js
@@ -6,31 +6,40 @@ export default function Trivia(){
 
     const [currentScore,updateScore]=useState(0);
     const[category,changeCategory]=useState();
+    const[question, setQuestion] = useState();
+    const[answer, setAnswer]=useState();
     
-    changeCategory('music')
-    function ajaxCall(){
-        $.ajax({
-            method: 'GET',
-            url: 'https://api.api-ninjas.com/v1/trivia?category=' + category,
-            headers: { 'X-Api-Key': '2Zy5oKzQec/6+/sQmRJbDw==qEMod1kazHRPkLjo'},
-            contentType: 'application/json',
-            success: function(result) {
-                console.log(result);
-            },
-            error: function ajaxError(jqXHR) {
-                console.error('Error: ', jqXHR.responseText);
-            }
-        });
+    function handleClick(category){
+       
+    let options = {
+    method: 'GET',
+    headers: { 'X-Api-Key': '2Zy5oKzQec/6+/sQmRJbDw==qEMod1kazHRPkLjo'}
     }
+  
+    let url = 'https://api.api-ninjas.com/v1/trivia?category=' + category;
+  
+  
+    fetch(url,options)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data)
+          setQuestion(data.question)
+          setAnswer(data.answer)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        }); 
+    }
+
     return(
     <div>
         <h1>Hello, Welcome to the Trivia Game</h1>
         <p>Choose a Category:</p>
-            <button onClick = {ajaxCall('general')}>General</button>
-            <button onClick = {ajaxCall('music')}>Music</button>
-            <button onClick = {ajaxCall('geography')}>Geography</button>
-            <button onClick = {ajaxCall('sportsleisure')}>Sports and Leisure</button>
-            <button onClick = {ajaxCall('historyholidays')}></button>
+            <button onClick = {handleClick('general')}>General</button>
+            <button onClick = {handleClick('music')}>Music</button>
+            <button onClick = {handleClick('geography')}>Geography</button>
+            <button onClick = {handleClick('sportsleisure')}>Sports and Leisure</button>
+            <button onClick = {handleClick('historyholidays')}>History and Holidays</button>
     </div>
 
     );
